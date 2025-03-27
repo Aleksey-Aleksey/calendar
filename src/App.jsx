e345ce7ac6ad3072d2b455a9732f9b96ca8d8ea2
o.ozerov.dev@gmail.com <o.ozerov.dev@gmail.com>
2025-03-28 00:03:37 +0200

    Enable mobile drag-and-drop support

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './styles/normalize.scss';
+import './App.scss';
 import { Calendar } from './components/calendar/Calendar';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { isTouchDevice } from './utils/isTouchDevice';
+
+const backend = isTouchDevice() ? TouchBackend : HTML5Backend;
 
 export const App = () => {
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={backend}>
       <div className="calendarApp">
         <Calendar />
       </div>
